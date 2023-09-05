fd4750a9bb1e5e08b7fd7144d69dd574c7f635cc
isaac uchechukwu edmund <edmundisaac16@gmail.com>
2023-09-05 09:48:43 +0100

    user exceptions

--- src/main/java/com/pryhmez/collabomain/properties/PropertyController.java
+++ src/main/java/com/pryhmez/collabomain/properties/PropertyController.java
@@ -1,4 +1,15 @@
 package com.pryhmez.collabomain.properties;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/property")
 public class PropertyController {
+
+    @PostMapping("/create")
+    public void createProperty () {
+
+    }
 }
