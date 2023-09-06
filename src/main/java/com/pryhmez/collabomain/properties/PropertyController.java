03903c36ecf73eb7b672f618c1d811d05c73ec39
isaac uchechukwu edmund <edmundisaac16@gmail.com>
2023-09-06 11:12:46 +0100

    get all properties

--- src/main/java/com/pryhmez/collabomain/properties/PropertyController.java
+++ src/main/java/com/pryhmez/collabomain/properties/PropertyController.java
@@ -1,5 +1,6 @@
 package com.pryhmez.collabomain.properties;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +11,11 @@ public class PropertyController {
 
     @PostMapping("/create")
     public void createProperty () {
+
+    }
+
+    @GetMapping("/getall")
+    public void getAllProperties() {
 
     }
 }
