71f7a9ab92cd8bae6986f9a0664346f57ba7d9ed
isaac uchechukwu edmund <edmundisaac16@gmail.com>
2023-09-21 09:16:32 +0100

    set investment status

---
+++ src/test/java/com/pryhmez/collabomain/propertyInvestments/PropertyInvestmentControllerTest.java
@@ -0,0 +1,24 @@
+package com.pryhmez.collabomain.propertyInvestments;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PropertyInvestmentControllerTest {
+
+    @Test
+    void invest() {
+    }
+
+    @Test
+    void getInvestmentsByUser() {
+    }
+
+    @Test
+    void getInvestmentsByProperty() {
+    }
+
+    @Test
+    void setInvestmentStatus() {
+    }
+}
